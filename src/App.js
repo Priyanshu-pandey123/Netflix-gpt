b787c468f4be22370f3ce30a494100edd0eba4ce
priyanshu pandey <prince678p@gmail.com>
2024-04-20 20:49:31 +0530

    INITIAL

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <h1 className='text-3xl text-green-300'>Priyanshu pandey</h1>
+    </div>
+  );
+}
+
+export default App;
